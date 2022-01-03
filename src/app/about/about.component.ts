17366ffd7e73e03e65779cafc0cca0c2b0197ccf
Thiago Castilho <thiagocastilho@me.com>
2022-01-03 17:23:30 -0300

    Core RxJs Concepts - Errors, Completion and Subscriptions

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,15 +10,21 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const interval$ = interval(1000);
+    // const interval$ = interval(1000);
 
-    interval$.subscribe((val) => console.log("stream 1 => ", val));
-    interval$.subscribe((val) => console.log("stream 2 => ", val));
+    // interval$.subscribe((val) => console.log("stream 1 => ", val));
+    // interval$.subscribe((val) => console.log("stream 2 => ", val));
 
     const interval2$ = timer(3000, 1000);
-    interval2$.subscribe((val) => console.log("stream 3 => ", val));
+    const sub = interval2$.subscribe((val) => console.log("stream 3 => ", val));
+
+    setTimeout(() => sub.unsubscribe(), 5000);
 
     const click$ = fromEvent(document, "click");
-    click$.subscribe((evt) => console.log(evt));
+    click$.subscribe(
+      (evt) => console.log(evt),
+      (err) => console.log(err),
+      () => console.log("completed")
+    );
   }
 }
