17f26f88f04122be85e06548e448e1034da38c94
Thiago Castilho <thiagocastilho@me.com>
2022-01-03 17:04:56 -0300

    What are streams?

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    document.addEventListener("click", (evt) => {
+      console.log(evt);
+    });
 
+    let counter = 0;
+
+    setInterval(() => {
+      console.log(counter);
+      counter++;
+    }, 1000);
+
+    setTimeout(() => {
+      console.log;
+    }, 3000);
+  }
 }
