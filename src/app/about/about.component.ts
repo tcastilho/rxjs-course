fd08924ecd6f1402528d5f15dc14f3c015642889
Thiago Castilho <thiagocastilho@me.com>
2022-01-03 17:16:19 -0300

    What is an RxJs Observable? A simple Explanation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { fromEvent, interval, timer } from "rxjs";
 
 @Component({
   selector: "about",
@@ -9,17 +10,15 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    document.addEventListener("click", (evt) => {
-      console.log(evt);
-      setTimeout(() => {
-        console.log;
-        let counter = 0;
+    const interval$ = interval(1000);
 
-        setInterval(() => {
-          console.log(counter);
-          counter++;
-        }, 1000);
-      }, 3000);
-    });
+    interval$.subscribe((val) => console.log("stream 1 => ", val));
+    interval$.subscribe((val) => console.log("stream 2 => ", val));
+
+    const interval2$ = timer(3000, 1000);
+    interval2$.subscribe((val) => console.log("stream 3 => ", val));
+
+    const click$ = fromEvent(document, "click");
+    click$.subscribe((evt) => console.log(evt));
   }
 }
